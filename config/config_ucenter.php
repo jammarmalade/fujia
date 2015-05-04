db3da524b2d64b340f7893b700c135742ffee503
jam <jam00@vip.qq.com>
2015-05-04 10:08:00 +0800

    后台权限管理

--- config/config_ucenter.php
+++ config/config_ucenter.php
@@ -6,14 +6,14 @@ define('UC_CONNECT', 'mysql');
 define('UC_DBHOST', 'localhost');
 define('UC_DBUSER', 'root');
 define('UC_DBPW', '123456');
-define('UC_DBNAME', 'fjtz_new');
+define('UC_DBNAME', 'fujia');
 define('UC_DBCHARSET', 'utf8');
-define('UC_DBTABLEPRE', '`fjtz_new`.z_ucenter_');
+define('UC_DBTABLEPRE', '`fujia`.z_ucenter_');
 define('UC_DBCONNECT', 0);
 
 define('UC_CHARSET', 'utf-8');
 define('UC_KEY', 'xbc4w5X1qcCca76at0k4McM5N6H7B1Pcd3l5Kbr0F4W7q2r2x1wcV6gfQ8C1n9i3');
-define('UC_API', 'http://fujiajr.com/admin/uc_server');
+define('UC_API', 'http://127.0.0.1/fujia/uc_server');
 define('UC_APPID', '1');
 define('UC_IP', '');
 define('UC_PPP', 20);
