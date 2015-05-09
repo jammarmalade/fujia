3a3b58e46c709a5e01a0757d43b52f23a474c27d
jam <jam00@vip.qq.com>
2015-05-09 18:14:47 +0800

    20150509backup

--- source/admincp/admincp_advmanage.php
+++ source/admincp/admincp_advmanage.php
@@ -10,40 +10,187 @@ if(!defined('IN_DISCUZ') || !defined('IN_ADMINCP')) {
 cpheader();
 
 
-$operation = $operation ? $operation : 'basic';
+$operation = $operation ? $operation : 'guide';
 
-if(!submitcheck('infosubmit')) {
 
-	showsubmenu('infomanage_'.$operation);
-	showformheader('infomanage', 'enctype');
-	showhiddenfields(array('operation' => $operation));
-	
-	if($operation == 'basic') {
-		
-		showtableheader('infomanage_basic');
-		showtablerow('class="header"', array('class="td25"','class="td24"','class="td31"','class="td30"','class="td30"'), array(
-			cplang('infomanage_title_id'),
-			cplang('infomanage_title_subject'),
-			cplang('infomanage_title_content'),
-			cplang('infomanage_title_time'),
-			cplang('operate'),
+
+if($operation == 'guide') {
+
+	if(!submitcheck('guidesubmit')) {
+		$_GET['anchor'] = in_array($_GET['anchor'], array('firstguide','loginguide')) ? $_GET['anchor'] : 'firstguide';
+		showsubmenu('advmanage_guide', array(
+			array('advmanage_firstguide', 'advmanage&operation=guide&anchor=firstguide', $_GET['anchor'] == 'firstguide'),
+			array('advmanage_loginguide', 'advmanage&operation=guide&anchor=loginguide', $_GET['anchor'] == 'loginguide'),
 		));
+		
+		showformheader('advmanage', 'enctype');
+		showhiddenfields(array('operation' => $operation));
+		
+		if($_GET['anchor']=='firstguide'){
+			showtableheader('advmanage_firstguide');
+		}elseif($_GET['anchor']=='loginguide'){
+			showtableheader('advmanage_loginguide');
+		}
+		showtagfooter('tbody');
+		showtablefooter();
 
-		$info=array(1,diconv('测试资讯','gbk'),diconv('资讯内容','gbk'),'2015-05-05 17:55:43',diconv('更新|删除','gbk'));
-		showtablerow('', '', $info);
+		showsubmit('guidesubmit', 'submit', '', (!empty($from) ? '<input type="hidden" name="from" value="'.$from.'">' : ''));
+		showtablefooter();
+		showformfooter();
+
+	} else {
+
+		cpmsg('update_success', 'action=advmanage&operation='.$operation.(!empty($_GET['anchor']) ? '&anchor='.$_GET['anchor'] : ''), 'succeed');
+	}
+
+}elseif($operation == 'popup'){
+	if(!submitcheck('popupsubmit')) {
+		showsubmenu('advmanage_popup');
+		
+		showformheader('advmanage', 'enctype');
+		showhiddenfields(array('operation' => $operation));
+
+		showtableheader('advmanage_popupinfo');
 
 		showtagfooter('tbody');
 		showtablefooter();
+
+		showsubmit('popupsubmit', 'submit', '', (!empty($from) ? '<input type="hidden" name="from" value="'.$from.'">' : ''));
+		showtablefooter();
+		showformfooter();
+
+	} else {
+
+		cpmsg('update_success', 'action=advmanage&operation='.$operation.(!empty($_GET['anchor']) ? '&anchor='.$_GET['anchor'] : ''), 'succeed');
+	}
+}elseif($operation == 'goldhome'){
+	if(!submitcheck('goldhomesubmit')) {
+
+		showsubmenu('advmanage_goldhome');
+		
+		showformheader('advmanage', 'enctype');
+		showhiddenfields(array('operation' => $operation));
+
+		showtableheader('advmanage_goldhomeinfo');
+
+		showtagfooter('tbody');
+		showtablefooter();
+
+		showsubmit('goldhomesubmit', 'submit', '', (!empty($from) ? '<input type="hidden" name="from" value="'.$from.'">' : ''));
+		showtablefooter();
+		showformfooter();
+
+	} else {
+
+		cpmsg('update_success', 'action=advmanage&operation='.$operation.(!empty($_GET['anchor']) ? '&anchor='.$_GET['anchor'] : ''), 'succeed');
 	}
 
-	showsubmit('infosubmit', 'submit', '', (!empty($from) ? '<input type="hidden" name="from" value="'.$from.'">' : ''));
-	showtablefooter();
-	showformfooter();
+}elseif($operation == 'product'){
+	if(!submitcheck('productsubmit')) {
 
-} else {
+		$_GET['anchor'] = in_array($_GET['anchor'], array('distribution','fund','stock','raise','openaccount')) ? $_GET['anchor'] : 'distribution';
+		showsubmenu('advmanage_product', array(
+			array('advmanage_distribution', 'advmanage&operation=product&anchor=distribution', $_GET['anchor'] == 'distribution'),
+			array('advmanage_fund', 'advmanage&operation=product&anchor=fund', $_GET['anchor'] == 'fund'),
+			array('advmanage_stock', 'advmanage&operation=product&anchor=stock', $_GET['anchor'] == 'stock'),
+			array('advmanage_raise', 'advmanage&operation=product&anchor=raise', $_GET['anchor'] == 'raise'),
+			array('advmanage_openaccount', 'advmanage&operation=product&anchor=openaccount', $_GET['anchor'] == 'openaccount'),
+		));
+		
+		showformheader('advmanage', 'enctype');
+		showhiddenfields(array('operation' => $operation));
+		
+		if($_GET['anchor']=='distribution'){
+			showtableheader('advmanage_distribution');
+		}elseif($_GET['anchor']=='fund'){
+			showtableheader('advmanage_fund');
+		}elseif($_GET['anchor']=='stock'){
+			showtableheader('advmanage_stock');
+		}elseif($_GET['anchor']=='raise'){
+			showtableheader('advmanage_raise');
+		}elseif($_GET['anchor']=='openaccount'){
+			showtableheader('advmanage_openaccount');
+		}
+		showtagfooter('tbody');
+		showtablefooter();
 
-	cpmsg('update_success', 'action=infomanage&operation='.$operation.(!empty($_GET['anchor']) ? '&anchor='.$_GET['anchor'] : ''), 'succeed');
+		showsubmit('productsubmit', 'submit', '', (!empty($from) ? '<input type="hidden" name="from" value="'.$from.'">' : ''));
+		showtablefooter();
+		showformfooter();
+
+	} else {
+
+		cpmsg('update_success', 'action=advmanage&operation='.$operation.(!empty($_GET['anchor']) ? '&anchor='.$_GET['anchor'] : ''), 'succeed');
+	}
+}elseif($operation == 'info'){
+	if(!submitcheck('infosubmit')) {
+
+		showsubmenu('advmanage_info');
+		
+		showformheader('advmanage', 'enctype');
+		showhiddenfields(array('operation' => $operation));
+
+		showtableheader('advmanage_infoinfo');
+
+		showtagfooter('tbody');
+		showtablefooter();
+
+		showsubmit('infosubmit', 'submit', '', (!empty($from) ? '<input type="hidden" name="from" value="'.$from.'">' : ''));
+		showtablefooter();
+		showformfooter();
+
+	} else {
+
+		cpmsg('update_success', 'action=advmanage&operation='.$operation.(!empty($_GET['anchor']) ? '&anchor='.$_GET['anchor'] : ''), 'succeed');
+	}
+
+}elseif($operation == 'discover'){
+	if(!submitcheck('discoversubmit')) {
+
+		showsubmenu('advmanage_discover');
+		
+		showformheader('advmanage', 'enctype');
+		showhiddenfields(array('operation' => $operation));
+
+		showtableheader('advmanage_discoverinfo');
+
+		showtagfooter('tbody');
+		showtablefooter();
+
+		showsubmit('discoversubmit', 'submit', '', (!empty($from) ? '<input type="hidden" name="from" value="'.$from.'">' : ''));
+		showtablefooter();
+		showformfooter();
+
+	} else {
+
+		cpmsg('update_success', 'action=advmanage&operation='.$operation.(!empty($_GET['anchor']) ? '&anchor='.$_GET['anchor'] : ''), 'succeed');
+	}
+
+}elseif($operation == 'system'){
+	if(!submitcheck('systemsubmit')) {
+
+		showsubmenu('advmanage_system');
+		
+		showformheader('advmanage', 'enctype');
+		showhiddenfields(array('operation' => $operation));
+
+		showtableheader('advmanage_systeminfo');
+
+		showtagfooter('tbody');
+		showtablefooter();
+
+		showsubmit('systemsubmit', 'submit', '', (!empty($from) ? '<input type="hidden" name="from" value="'.$from.'">' : ''));
+		showtablefooter();
+		showformfooter();
+
+	} else {
+
+		cpmsg('update_success', 'action=advmanage&operation='.$operation.(!empty($_GET['anchor']) ? '&anchor='.$_GET['anchor'] : ''), 'succeed');
+	}
 }
+
+
+
 
 
 ?>
--- source/admincp/admincp_trademanage.php
+++ source/admincp/admincp_trademanage.php
@@ -10,31 +10,187 @@ if(!defined('IN_DISCUZ') || !defined('IN_ADMINCP')) {
 cpheader();
 
 
-$operation = $operation ? $operation : 'basic';
+$operation = $operation ? $operation : 'fundinvestment';
 
-if(!submitcheck('goldsubmit')) {
 
-	showsubmenu('goldmanage_'.$operation);
-	showtips('goldmanage_tips');
-	showformheader('usermanage', 'enctype');
-	showhiddenfields(array('operation' => $operation));
-	
-	if($operation == 'basic') {
+
+if($operation == 'fundinvestment') {
+
+	if(!submitcheck('investmentsubmit')) {
+		$_GET['anchor'] = in_array($_GET['anchor'], array('investmenta','investmentb')) ? $_GET['anchor'] : 'investmenta';
+		showsubmenu('trademanage_investment', array(
+			array('trademanage_investmenta', 'trademanage&operation=fundinvestment&anchor=investmenta', $_GET['anchor'] == 'investmenta'),
+			array('trademanage_investmentb', 'trademanage&operation=fundinvestment&anchor=investmentb', $_GET['anchor'] == 'investmentb'),
+		));
 		
-		showtableheader('goldmanage_basic');
+		showformheader('trademanage', 'enctype');
+		showhiddenfields(array('operation' => $operation));
+		
+		if($_GET['anchor']=='investmenta'){
+			showtableheader('trademanage_investmenta');
+		}elseif($_GET['anchor']=='investmentb'){
+			showtableheader('trademanage_investmentb');
+		}
+		showtagfooter('tbody');
+		showtablefooter();
+
+		showsubmit('investmentsubmit', 'submit', '', (!empty($from) ? '<input type="hidden" name="from" value="'.$from.'">' : ''));
+		showtablefooter();
+		showformfooter();
+
+	} else {
+
+		cpmsg('update_success', 'action=trademanage&operation='.$operation.(!empty($_GET['anchor']) ? '&anchor='.$_GET['anchor'] : ''), 'succeed');
+	}
+
+}elseif($operation == 'fundbuy'){
+	if(!submitcheck('fundbuysubmit')) {
+		showsubmenu('trademanage_fundbuy');
+		
+		showformheader('trademanage', 'enctype');
+		showhiddenfields(array('operation' => $operation));
+
+		showtableheader('trademanage_fundbuyinfo');
 
 		showtagfooter('tbody');
 		showtablefooter();
+
+		showsubmit('fundbuysubmit', 'submit', '', (!empty($from) ? '<input type="hidden" name="from" value="'.$from.'">' : ''));
+		showtablefooter();
+		showformfooter();
+
+	} else {
+
+		cpmsg('update_success', 'action=trademanage&operation='.$operation.(!empty($_GET['anchor']) ? '&anchor='.$_GET['anchor'] : ''), 'succeed');
+	}
+}elseif($operation == 'stockbuy'){
+	if(!submitcheck('stockbuysubmit')) {
+
+		showsubmenu('trademanage_stockbuy');
+		
+		showformheader('trademanage', 'enctype');
+		showhiddenfields(array('operation' => $operation));
+
+		showtableheader('trademanage_stockbuyinfo');
+
+		showtagfooter('tbody');
+		showtablefooter();
+
+		showsubmit('stockbuysubmit', 'submit', '', (!empty($from) ? '<input type="hidden" name="from" value="'.$from.'">' : ''));
+		showtablefooter();
+		showformfooter();
+
+	} else {
+
+		cpmsg('update_success', 'action=trademanage&operation='.$operation.(!empty($_GET['anchor']) ? '&anchor='.$_GET['anchor'] : ''), 'succeed');
 	}
 
-	showsubmit('goldsubmit', 'submit', '', (!empty($from) ? '<input type="hidden" name="from" value="'.$from.'">' : ''));
-	showtablefooter();
-	showformfooter();
+}elseif($operation == 'raisebuy'){
+	if(!submitcheck('raisebuysubmit')) {
 
-} else {
+		showsubmenu('trademanage_raisebuy');
+		
+		showformheader('trademanage', 'enctype');
+		showhiddenfields(array('operation' => $operation));
 
-	cpmsg('update_success', 'action=goldmanage&operation='.$operation.(!empty($_GET['anchor']) ? '&anchor='.$_GET['anchor'] : ''), 'succeed');
+		showtableheader('trademanage_raisebuyinfo');
+
+		showtagfooter('tbody');
+		showtablefooter();
+
+		showsubmit('raisebuysubmit', 'submit', '', (!empty($from) ? '<input type="hidden" name="from" value="'.$from.'">' : ''));
+		showtablefooter();
+		showformfooter();
+
+	} else {
+
+		cpmsg('update_success', 'action=trademanage&operation='.$operation.(!empty($_GET['anchor']) ? '&anchor='.$_GET['anchor'] : ''), 'succeed');
+	}
+
+}elseif($operation == 'raisesupport'){
+	if(!submitcheck('raisesupportsubmit')) {
+
+		showsubmenu('trademanage_raisesupport');
+		
+		showformheader('trademanage', 'enctype');
+		showhiddenfields(array('operation' => $operation));
+
+		showtableheader('trademanage_raisesupportinfo');
+
+		showtagfooter('tbody');
+		showtablefooter();
+
+		showsubmit('raisesupportsubmit', 'submit', '', (!empty($from) ? '<input type="hidden" name="from" value="'.$from.'">' : ''));
+		showtablefooter();
+		showformfooter();
+
+	} else {
+
+		cpmsg('update_success', 'action=trademanage&operation='.$operation.(!empty($_GET['anchor']) ? '&anchor='.$_GET['anchor'] : ''), 'succeed');
+	}
+
+}elseif($operation == 'distribution'){
+	if(!submitcheck('distributionsubmit')) {
+		$_GET['anchor'] = in_array($_GET['anchor'], array('stockindex','stock')) ? $_GET['anchor'] : 'stockindex';
+		showsubmenu('trademanage_distribution', array(
+			array('trademanage_stockindex', 'trademanage&operation=distribution&anchor=stockindex', $_GET['anchor'] == 'stockindex'),
+			array('trademanage_stock', 'trademanage&operation=distribution&anchor=stock', $_GET['anchor'] == 'stock'),
+		));
+
+		showformheader('trademanage', 'enctype');
+		showhiddenfields(array('operation' => $operation));
+
+		if($_GET['anchor']=='stockindex'){
+			showtableheader('trademanage_stockindex');
+		}elseif($_GET['anchor']=='stock'){
+			showtableheader('trademanage_stock');
+		}
+
+		showtagfooter('tbody');
+		showtablefooter();
+
+		showsubmit('distributionsubmit', 'submit', '', (!empty($from) ? '<input type="hidden" name="from" value="'.$from.'">' : ''));
+		showtablefooter();
+		showformfooter();
+
+	} else {
+
+		cpmsg('update_success', 'action=trademanage&operation='.$operation.(!empty($_GET['anchor']) ? '&anchor='.$_GET['anchor'] : ''), 'succeed');
+	}
+
+}elseif($operation == 'openaccount'){
+	if(!submitcheck('openaccountsubmit')) {
+
+		$_GET['anchor'] = in_array($_GET['anchor'], array('accountstock','accountfutures')) ? $_GET['anchor'] : 'accountstock';
+		showsubmenu('trademanage_openaccount', array(
+			array('trademanage_accountstock', 'trademanage&operation=openaccount&anchor=accountstock', $_GET['anchor'] == 'accountstock'),
+			array('trademanage_accountfutures', 'trademanage&operation=openaccount&anchor=accountfutures', $_GET['anchor'] == 'accountfutures'),
+		));
+
+		showformheader('trademanage', 'enctype');
+		showhiddenfields(array('operation' => $operation));
+
+		if($_GET['anchor']=='accountstock'){
+			showtableheader('trademanage_accountstock');
+		}elseif($_GET['anchor']=='accountfutures'){
+			showtableheader('trademanage_accountfutures');
+		}
+
+		showtagfooter('tbody');
+		showtablefooter();
+
+		showsubmit('openaccountsubmit', 'submit', '', (!empty($from) ? '<input type="hidden" name="from" value="'.$from.'">' : ''));
+		showtablefooter();
+		showformfooter();
+
+	} else {
+
+		cpmsg('update_success', 'action=trademanage&operation='.$operation.(!empty($_GET['anchor']) ? '&anchor='.$_GET['anchor'] : ''), 'succeed');
+	}
 }
+
+
+
 
 
 ?>
